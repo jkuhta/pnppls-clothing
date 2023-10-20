016358877e632c6a571de1ceeecf9ca27208e37c
jkuhta <jan.kuhta@gmail.com>
2023-10-21 00:25:19 +0200

    login component

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
@@ -8,26 +8,14 @@ const PWD_REGEX =
 const USER_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 const Login = () => {
-  const userRef = useRef();
-  const errRef = useRef();
-
   const [user, setUser] = useState("");
   const [validName, setValidName] = useState(false);
 
   const [pwd, setPwd] = useState("");
   const [validPwd, setValidPwd] = useState(false);
 
-  const [matchPwd, setMatchPwd] = useState("");
-  const [validMatch, setvalidMatch] = useState(false);
-
-  const [checkBox, setCheckbox] = useState(false);
-
   const [errMsg, setErrMsg] = useState("");
   const [success, setSuccess] = useState(false);
-
-  useEffect(() => {
-    userRef.current.focus();
-  }, []);
 
   useEffect(() => {
     const result = USER_REGEX.test(user);
@@ -37,18 +25,11 @@ const Login = () => {
   useEffect(() => {
     const result = PWD_REGEX.test(pwd);
     setValidPwd(result);
-    const match = pwd === matchPwd;
-    setvalidMatch(match);
-  }, [pwd, matchPwd]);
+  }, [pwd]);
 
   useEffect(() => {
     setErrMsg("");
-  }, [user, pwd, matchPwd]);
-
-  const handleCheckBox = () => {
-    setCheckbox(!checkBox);
-    console.log(checkBox);
-  };
+  }, [user, pwd]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -57,6 +38,8 @@ const Login = () => {
     const v2 = PWD_REGEX.test(pwd);
     if (!v1 || !v2) {
       setErrMsg("Invalid Entry");
+      console.log(errMsg);
+
       return;
     }
 
@@ -68,36 +51,9 @@ const Login = () => {
   return (
     <div className="login pineapples">
       <div className="login-container">
-        <p
-          ref={errRef}
-          className={errMsg ? "errmsg" : "offscreen"}
-          aria-live="assertive"
-        >
-          {errMsg}
-        </p>
-        <h1 className="section-title login-title">Create Account</h1>
+        <h1 className="section-title login-title">Log In</h1>
         <form onSubmit={handleSubmit}>
           <div className="login-fields">
-            <input
-              autoComplete="off"
-              ref={userRef}
-              type="text"
-              placeholder="First Name"
-            ></input>
-            <input
-              autoComplete="off"
-              ref={userRef}
-              type="text"
-              placeholder="Last Name"
-            ></input>
-            <span className={validName ? "valid login-sign" : "hide"}>
-              <i className="fa-solid fa-check"></i>
-            </span>
-            <span
-              className={validName || !user ? "hide" : "invalid login-sign"}
-            >
-              <i className="fa-solid fa-x"></i>
-            </span>
             <input
               type="email"
               id="username"
@@ -107,12 +63,7 @@ const Login = () => {
               aria-describedby="uidnote"
               placeholder="Email Address"
             />
-            <span className={validPwd ? "valid login-sign" : "hide"}>
-              <i className="fa-solid fa-check"></i>
-            </span>
-            <span className={validPwd || !pwd ? "hide" : "invalid login-sign"}>
-              <i className="fa-solid fa-x"></i>
-            </span>
+
             <input
               type="password"
               id="password"
@@ -122,58 +73,15 @@ const Login = () => {
               aria-describedby="pwdnote"
               placeholder="Password"
             />
-            <i className="bx bx-info-circle login-tooltip-icon"></i>{" "}
-            <p className="login-tooltip-text">
-              8 to 24 characters.
-              <br />
-              Must include uppercase and lowercase letters, a number and a
-              special character.
-            </p>
-            <span
-              className={validMatch && matchPwd ? "valid login-sign" : "hide"}
-            >
-              <i className="fa-solid fa-check"></i>
-            </span>
-            <span
-              className={
-                validMatch || !matchPwd ? "hide" : "invalid login-sign"
-              }
-            >
-              <i className="fa-solid fa-x"></i>
-            </span>
-            <input
-              type="password"
-              id="confirm_pwd"
-              required
-              aria-invalid={validMatch ? "false" : "true"}
-              aria-describedby="confirmnote"
-              placeholder="Confirm Password"
-            />
           </div>
-          <div className="login-agree">
-            <input
-              type="checkbox"
-              required
-              name=""
-              id=""
-              className="login-checkbox"
-              onChange={handleCheckBox}
-            ></input>
-            <p>By continuing I agree to terms of use & privacy policy.</p>
-          </div>
-          <button
-            disabled={
-              !validName || !validPwd || !validMatch || !checkBox ? true : false
-            }
-          >
-            Sign up
-          </button>
+
+          <button disabled={!user || !pwd ? true : false}>Log in</button>
         </form>
-        <p className="login-login">
-          Already have an account? {"  "}
-          <Link to="/login" className="login-login-btn link">
+        <p className="login-footer">
+          Don't have an account? {"  "}
+          <Link to="/signup" className="login-footer-btn link">
             {" "}
-            <span>Login here</span>
+            <span>Sign-up here</span>
           </Link>
           .
         </p>
