a75fd2b18189c30dc1f8828f8e08c0b973e727e7
jkuhta <jan.kuhta@gmail.com>
2023-10-21 15:53:56 +0200

    add filter

--- src/pages/shopCategory/ShopCategory.jsx
+++ src/pages/shopCategory/ShopCategory.jsx
@@ -5,16 +5,19 @@ import Item from "../../components/item/Item";
 import Dropdown from "../../components/dropdown/Dropdown";
 import { useState } from "react";
 import { useEffect } from "react";
+import Filter from "../../components/filter/Filter";
 
 const ShopCategory = ({ category }) => {
   const { all_product } = useContext(ShopContext);
 
-  const filtered_products = all_product.filter(
+  const category_products = all_product.filter(
     (item) => item.category === category
-  ); // Filter items by category
+  );
 
   const [selectedItem, setSelectedItem] = useState("rating");
   const [sortedItems, setSortedItems] = useState(all_product);
+  const [showFilterMenu, setShowFilterMenu] = useState(false);
+  const [filteredItems, setFilteredItems] = useState(category_products);
 
   const handleItemClick = (item) => {
     setSelectedItem(item);
@@ -33,7 +36,7 @@ const ShopCategory = ({ category }) => {
         price_down: "new_price",
       };
       const sortProperty = types[type];
-      const sorted = [...all_product].sort((a, b) =>
+      const sorted = [...filteredItems].sort((a, b) =>
         selectedItem === "price_up"
           ? a[sortProperty] - b[sortProperty]
           : b[sortProperty] - a[sortProperty]
@@ -42,21 +45,26 @@ const ShopCategory = ({ category }) => {
     };
 
     sortArray(selectedItem);
-  }, [selectedItem, all_product]);
+  }, [selectedItem, filteredItems]);
 
   return (
     <div className="shop-category pineapples">
       <div className="shop-category-title">{category}'s Collection</div>
       <div className="shop-category-settings">
         <div className="shop-category-filter">
-          <button>
-            <i className="bx bx-filter-alt"></i> Filter
-          </button>
+          <Filter
+            setShowFilterMenu={setShowFilterMenu}
+            showFilterMenu={showFilterMenu}
+            all_product={all_product}
+            filteredItems={filteredItems}
+            setFilteredItems={setFilteredItems}
+            category={category}
+          />
         </div>
         <div className="shop-category-settings-right">
           <div className="shop-category-count">
             <p>
-              {filtered_products.length} of {filtered_products.length} products
+              {filteredItems.length} of {category_products.length} products
             </p>
           </div>
           <div className="shop-category-sort">
@@ -67,12 +75,16 @@ const ShopCategory = ({ category }) => {
           </div>
         </div>
       </div>
+      {(sortedItems.length > 0 && (
+        <div className="shop-category-products">
+          {sortedItems.map((item, index) => {
+            return (
+              item.category === category && <Item key={index} item={item} />
+            );
+          })}
+        </div>
+      )) || <div className="shop-category-no-results">No results.</div>}
 
-      <div className="shop-category-products">
-        {sortedItems.map((item, index) => {
-          return item.category === category && <Item key={index} item={item} />;
-        })}
-      </div>
       {/* <div className="shop-category-loadmore">Explore More</div> */}
     </div>
   );
