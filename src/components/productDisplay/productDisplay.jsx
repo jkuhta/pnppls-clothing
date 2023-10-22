c8d1001bdfbcd87f858f91c2b1165caf44edd265
jkuhta <jan.kuhta@gmail.com>
2023-10-22 17:52:19 +0200

    add responsivnes 3

--- src/components/productDisplay/productDisplay.jsx
+++ src/components/productDisplay/productDisplay.jsx
@@ -51,13 +51,7 @@ const ProductDisplay = (props) => {
             <img src={product.image} alt="" />
             <img src={product.image} alt="" />
           </div>
-          <div className="productdisplay-img">
-            <img
-              className="productdisplay-main-img"
-              src={product.image}
-              alt=""
-            />
-          </div>
+          <img className="productdisplay-main-img" src={product.image} alt="" />
         </div>
         <div className="productdisplay-right">
           <div className="productdisplay-labels">
