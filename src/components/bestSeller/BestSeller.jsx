beaefde2d2a0217cf44768b56c93a58605bc3f95
jkuhta <jan.kuhta@gmail.com>
2023-10-23 12:39:35 +0200

    add bestSeller scroll

--- src/components/bestSeller/BestSeller.jsx
+++ src/components/bestSeller/BestSeller.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./BestSeller.css";
 import all_product from "../../assets/all_product";
 import Item from "../item/Item";
@@ -6,10 +6,34 @@ import { Link } from "react-router-dom";
 
 const BestSeller = ({ category }) => {
   const filteredAndSortedItems = all_product
-    .filter((item) => item.category === category) // Filter items by category
-    .sort((a, b) => b.rating - a.rating); // Sort by rating in descending order
+    .filter((item) => item.category === category)
+    .sort((a, b) => b.rating - a.rating);
 
-  const top4Items = filteredAndSortedItems.slice(0, 4);
+  const top8Items = filteredAndSortedItems.slice(0, 8);
+
+  const scrollContainerRef = useRef(null); // Create a ref for the scroll container
+  const itemRef = useRef(null); // Create a ref for the Item component
+  const [itemWidth, setItemWidth] = useState(0); // State to store item width
+
+  useEffect(() => {
+    if (itemRef.current) {
+      // Measure the width of the rendered Item component
+      const width = itemRef.current.offsetWidth;
+      setItemWidth(width);
+    }
+  }, [top8Items]);
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft -= itemWidth + 20;
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollLeft += itemWidth + 20;
+    }
+  };
 
   return (
     <div className="bestSeller">
@@ -25,11 +49,26 @@ const BestSeller = ({ category }) => {
         </Link>
       </div>
 
-      <div className="bestSeller-items-container">
+      <div className="bestSeller-items-container" ref={scrollContainerRef}>
         <div className="bestSeller-items">
-          {top4Items.map((item, index) => {
-            return <Item key={index} item={item}></Item>;
+          {top8Items.map((item, index) => {
+            return (
+              <div className="bestSeller-item" key={index} ref={itemRef}>
+                <Item item={item} />
+              </div>
+            );
           })}
+        </div>
+      </div>
+      <div className="bestSeller-arrows">
+        <div className="arrow left" onClick={scrollLeft}>
+          <i className="fas fa-chevron-left"></i>
+        </div>
+        <span className="item-count">
+          {/* Display the current item count here */}
+        </span>
+        <div className="arrow right" onClick={scrollRight}>
+          <i className="fas fa-chevron-right"></i>
         </div>
       </div>
     </div>
